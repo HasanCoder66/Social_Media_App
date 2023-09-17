00c51d1af81428c9ca155d634f2a4de8ea06f9bb
66CoderHasan <hasanashraf874@gmail.com>
2023-09-17 01:04:33 -0700

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import Home from '../src/pages/home/home'
+import TopBar from '../src/components/topbar/topbar'
+// import {Person} from '@material-ui/icons-'
 
-function App() {
+function App () {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TopBar/>
+    {/* <Home/> */}
+    {/* <Person/> */}
+    
+    </>
+  )
 }
 
 export default App;
